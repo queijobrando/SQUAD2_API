eddc49df774214aacb2c32fde07390c95513bbbb
Tiago Souza <tiagoadesouza21@gmail.com>
2025-05-21 09:31:11 -0300

    Adicionando summary, descrição e tag do Swagger nos metodos

--- src/main/java/com/example/squad2_suporte/Controller/CadastroLaminaController.java
+++ src/main/java/com/example/squad2_suporte/Controller/CadastroLaminaController.java
@@ -4,6 +4,7 @@ import com.example.squad2_suporte.Amostras.mapper.LaminaMapper;
 import com.example.squad2_suporte.dto.lamina.LaminaDto;
 import com.example.squad2_suporte.dto.lamina.RetornoLaminaDto;
 import com.example.squad2_suporte.service.LaminaService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,6 +20,7 @@ public class CadastroLaminaController {
     @Autowired
     private LaminaMapper laminaMapper;
 
+    @Operation(summary = "Cadastrar Lamina", description = "Metodo para cadastrar uma nova Lamina", tags = "Gerenciar Lamina")
     @PostMapping
     public ResponseEntity<RetornoLaminaDto> cadastrarLamina(@RequestBody LaminaDto dto, UriComponentsBuilder uriComponentsBuilder){
         var novaLamina = laminaService.cadastrarLamina(dto);
@@ -28,12 +30,14 @@ public class CadastroLaminaController {
         return ResponseEntity.created(uri).body(laminaMapper.entidadeParaRetorno(novaLamina)); // 201 CREATED
     }
 
+    @Operation(summary = "Deletar Lamina", description = "Metodo para deletar uma lamina existente pelo numero do protocolo", tags = "Gerenciar Lamina")
     @DeleteMapping("/{protocolo}")
     public ResponseEntity<String> removerLamina(@PathVariable Long protocolo) {
         laminaService.deletarLamina(protocolo);
         return ResponseEntity.ok("Lamina removida!");
     }
 
+    @Operation(summary = "Buscar Lamina", description = "Metodo para buscar e exibir informações de uma lamina", tags = "Gerenciar Lamina")
     @GetMapping("/{protocolo}")
     public ResponseEntity<RetornoLaminaDto> buscarLamina(@PathVariable Long protocolo){
         var lamina = laminaService.buscarLamina(protocolo);
--- src/main/java/com/example/squad2_suporte/Controller/GerenciarLoteController.java
+++ src/main/java/com/example/squad2_suporte/Controller/GerenciarLoteController.java
@@ -4,6 +4,7 @@ import com.example.squad2_suporte.Amostras.mapper.LoteMapper;
 import com.example.squad2_suporte.dto.lote.LoteDto;
 import com.example.squad2_suporte.dto.lote.RetornoLoteDto;
 import com.example.squad2_suporte.service.LoteService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -21,6 +22,7 @@ public class GerenciarLoteController {
     @Autowired
     private LoteMapper loteMapper;
 
+    @Operation(summary = "Criar Lote", description = "Metodo para criar um novo lote", tags = "Gerenciar Lote")
     @PostMapping("criar")
     public ResponseEntity<RetornoLoteDto> criarLote(@RequestBody LoteDto loteDto, UriComponentsBuilder uriComponentsBuilder){
         var lote = loteService.cadastrarLote(loteDto);
@@ -30,6 +32,7 @@ public class GerenciarLoteController {
         return ResponseEntity.created(uri).body(loteMapper.entidadeParaRetorno(lote));
     }
 
+    @Operation(summary = "Listar Lotes", description = "Metodo para listar lotes cadastrados", tags = "Gerenciar Lote")
     @GetMapping("lotesCadastrados")
     public ResponseEntity<List<RetornoLoteDto>> listarLotes(){
         var listaLotes = loteService.listarLotes();
